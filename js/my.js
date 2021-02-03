1e13ba64a3f3faf889be8cdc9f02c494319c6154
Patrick of AdDU <patrick.paasa@gmail.com>
2021-02-03 16:36:23 +0800

    Discussed/Demonstrated JS Objects

--- js/my.js
+++ js/my.js
@@ -1,2 +1,21 @@
-console.log('Hello World');
-let variable = 100;
+const tweet = {
+    user: "Brie",
+    post: "Giving stand up comedy a go",
+    replies: [
+        { user: "al", message: "Agree on this" },
+        { user: "Brie", message: "Are you a comedian?" },
+    ],
+    retweets: 15,
+    hearts: 123,
+    retweet: function () {
+        // repost / retweet
+        console.log("retweeting");
+    },
+    showReplies: function () {
+        for (reply of this.replies) {
+            console.log(`${reply.user} = ${reply.message}`);
+        }
+    }
+}
+
+tweet.showReplies();
